06df8186358e5739011cf79c55d644696bb4110d
huan <hejiahuan11@h-partners.com>
2026-04-09 11:18:47 +0800

    同步700RC3版本

--- build/Makefile.sh
+++ build/Makefile.sh
@@ -498,7 +498,8 @@ func_download_3rdparty()
     cd open_source
 
     # openGauss third_party repo (contains zstd / openssl / huawei_secure_c, etc.)
-    git clone https://gitcode.com/opengauss/openGauss-third_party.git -b master --depth 1
+    git clone https://gitcode.com/opengauss/openGauss-third_party.git -b 7.0.0-RC3 --depth 1
+
     # pcre2: use src-openeuler repo
     git clone https://gitcode.com/src-openeuler/pcre2.git -b openEuler-24.03-LTS-SP3
 
